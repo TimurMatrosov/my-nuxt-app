e5d6f7b039c430ccc01a77333d87e39b304fc52f
Timur Matrosov <timurmatrosov@MacBookPro.lan>
2025-04-03 10:41:48 +0500

    Update baseURL in nuxt.config.ts for GitHub Pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt'],
+  app: {
+    baseURL: '/my-nuxt-app/', // Set the base URL to match your GitHub repository name
+  },
 
   // Enable server-side rendering
   ssr: true,
